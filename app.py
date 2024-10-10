d45f8c767acdae06cef7d74c9d67843c8778aa42
ShmuelRozental <rozentalshmuel@gmail.com>
2024-10-10 16:09:12 +0300

    update app and requierment

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from config import Config
+from dotenv import load_dotenv
 from flask import Flask
+from dal.connect import database
 
-
+load_dotenv()
 
 app = Flask(__name__)
 
-app.config.from_object(Config)
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+app.config.from_object('config.Config')
+
+
+from routes.insert_routes import accident_bp
+app.register_blueprint(accident_bp)
 
 
 if __name__ == '__main__':
--- config.py
+++ config.py
@@ -1,9 +1,8 @@
-
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 class Config:
-    DEBUG = os.getenv('FLASK_DEBUG', 'production') == 'development'
-    MONGO_URI = os.getenv('DATABASE_URL', 'mongodb://localhost:27017/flask_db')
+    DEBUG = os.getenv('FLASK_ENV') == 'development'
+    MONGO_URI = os.getenv('DATABASE_URL', 'mongodb://localhost:27017/TrafficAccidentsDB')
