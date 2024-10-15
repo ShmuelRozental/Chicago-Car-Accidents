b1fb7818823f912cd81a7a297a5cdc7be8d686db
ShmuelRozental <rozentalshmuel@gmail.com>
2024-10-15 19:51:36 +0300

    modify gitignore

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Config:
-    DEBUG = os.getenv('FLASK_ENV') == 'development'
-    MONGO_URI = os.getenv('MONGO_URI', 'mongodb://mongodb:27017/TrafficAccidentsDB')
